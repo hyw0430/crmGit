e4d4a3485e6a99f221821b89491a53ac76eaa2dc
nickyhou <274140647@qq.com>
2017-07-03 16:20:32 +0800

    中文

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -2,8 +2,8 @@ package com.atguigu.github.test;
 
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("hello,github·şÎñÆ÷");
-		System.out.println("hello,github·şÎñÆ÷02");
+		System.out.println("hello,githubæœåŠ¡å™¨");
+		System.out.println("hello,githubæœåŠ¡å™¨02");
 	}
 
 }
