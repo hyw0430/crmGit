871a1cf76466cda8146d009787bf9145f0297bb6
hyw0430 <274140647@qq.com>
2017-07-03 16:21:58 +0800

    to local
    to local

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -4,6 +4,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello,github服务器");
 		System.out.println("hello,github服务器02");
+		System.out.println("github 服务器 response to local");
 	}
 
 }
